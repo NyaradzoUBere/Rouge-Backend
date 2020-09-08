4df90f7a0dcb7e99ddd2bc8bf73d7ccb4935a265
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-08 16:01:26 -0600

    render only sample of 50 seeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
     def index
-        @products = Product.all
+        @products = Product.all.sample(50)
         render json: @products
+    end
+
+    def show
+        @product = Product.find(params[:id])
+        render json: @product
     end
 end
