696f0dde95151df5a745754bd96376357ebb43fb
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-08 14:07:26 -0600

    add rest-client gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create_table "products", force: :cascade do |t|
+#     t.string "brand"
+#     t.string "name"
+#     t.float "price"
+#     t.string "price_sign"
+#     t.string "currency"
+#     t.string "image_link"
+#     t.string "product_link"
+#     t.string "website_link"
+#     t.text "description"
+#     t.string "category"
+#     t.string "product_type"
+#     t.string "product_colors", default: [], array: true
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
+
+base_url = "http://makeup-api.herokuapp.com/api/v1/products.json"
+
+data = RestClient.get(base_url)
+
+binding.pry
